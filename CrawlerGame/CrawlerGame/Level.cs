c06da44c4167b21def4ed37de11d491992817f45
Nitesh Parmar <niteshp2007@gmail.com>
2019-03-27 15:20:49 +0000

    final touches

---
+++ CrawlerGame/CrawlerGame/Level.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrawlerGame
+{
+    class Level
+    {
+        private readonly Invader[] _invaders;
+
+        public Tower[] Towers { get; set; }
+
+        public Level(Invader[] invaders)
+        {
+            _invaders = invaders;
+        }
+        //Returns true if the player wins, false otherwise
+        public bool Play()
+        {
+            //run until all invaders are dead or invader reaches the end
+            int reamainingInvaders = _invaders.Length;
+
+            while (reamainingInvaders > 0)
+            {
+                //Each tower has opp to fire on invaders
+                foreach (Tower tower in Towers)
+                {
+                    tower.FireOnInvaders(_invaders);
+                }
+
+
+
+                // count and move the invaders that still active
+                reamainingInvaders = 0;
+                foreach (Invader invader in _invaders)
+                {
+                    if (invader.IsActive)
+                    {
+                        invader.Move();
+
+                        if (invader.HasScored)
+                        {
+                            return false;
+                        }
+                        reamainingInvaders++;
+                    }
+                }
+
+            }
+
+            return true;
+        }
+    }
+}
--- CrawlerGame/CrawlerGame/Tower.cs
+++ CrawlerGame/CrawlerGame/Tower.cs
@@ -9,11 +9,23 @@ namespace CrawlerGame
     class Tower
     {
         private readonly MapLocation _location;
+        private const int _range = 1;
+        private const int _power = 1;
+        private const double _accuracy = 0.75;
+
+
+        private static readonly Random _random = new Random();
+
 
         public Tower(MapLocation location)
         {
 
             _location = location;
+        }
+
+        public bool IsSucessfulShot()
+        {
+            return _random.NextDouble() < _accuracy;
         }
 
         public void FireOnInvaders(Invader[] invaders)
@@ -35,9 +47,22 @@ namespace CrawlerGame
             */
             foreach (Invader invader in invaders)
             {
-                if (invader.IsActive && _location.InRangeOf(invader.Location, 1))
+                if (invader.IsActive && _location.InRangeOf(invader.Location, _range))
                 {
-                    invader.DecreaseHealth(1);
+                    if (IsSucessfulShot())
+                    {
+                        invader.DecreaseHealth(_power);
+                        Console.WriteLine("Shot at and hit an invader");
+                        if (invader.IsNeutralized)
+                        {
+                            Console.WriteLine("Neutralised and invader");
+                        }
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Shot at and missed an invader");
+                    }
                     break;
                 }
             }
