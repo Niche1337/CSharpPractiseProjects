3c4499f5cfad9a589b147a2846bae031dddf591f
Nitesh Parmar <niteshp2007@gmail.com>
2019-10-11 23:19:09 +0100

    Finished

--- MarginalTax/MarginalTax/Program.cs
+++ MarginalTax/MarginalTax/Program.cs
@@ -19,20 +19,69 @@ namespace MarginalTax
         // 46351 - 150000 = 40%
         // > 150000 = 45%
 
-        static void EndProg()
+
+          // if (num > 10000 && num <= 30000) {
+          //  tax += Math.floor(num - 10000 * 0.1);
+          //}
+
+          //if (num > 30000 && num <= 100000) {
+          //  tax += Math.floor(num - 30000 * 0.25) + 2000;
+          //}
+
+          //if (num > 100000) {
+          //  tax += Math.floor(num - 100000 * 0.4) + 19500;
+          //}
+          //return tax;
+
+
+        static string EndProg()
         {
             // The code provided will print ‘Hello World’ to the console.
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Press any key to close the application");
-            Console.ReadKey();
+            Console.WriteLine("Do you want to use the calculator Y/n");
+            return Console.ReadLine();
+        }
+
+        static void Tax()
+        {
+
+            double preTaxValue;
+            double newTaxValue = 0;
+            Console.WriteLine("Enter your current salary. ");
+            preTaxValue = double.Parse(Console.ReadLine());
+
+            ///calculations are wrong buts its just for practise
+
+            if (preTaxValue > 11850 && preTaxValue <= 46350)
+            {
+                newTaxValue += preTaxValue - 11850 * 0.20;
+            }
+
+            if (preTaxValue > 46351 && preTaxValue <= 150000)
+            {
+                newTaxValue += (preTaxValue - 46350 * 0.40);
+            }
+
+            if (preTaxValue > 150000)
+            {
+                newTaxValue += (preTaxValue - 150000 * 0.45);
+            }
+
+            Console.WriteLine($"Your after tax amount is {newTaxValue}.\nThank you for using this calculator.");
+
         }
 
         static void Main(string[] args)
         {
 
+            while (EndProg().ToLower() != "n")
+            {
+                Tax();
+            }
 
 
-            EndProg();
+
+            
         }
     }
 }
